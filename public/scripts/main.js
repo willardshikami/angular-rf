74d9ea243d7c503723b55337a15041ddf7b62067
willardshikami <willardshikami@gmail.com>
2018-03-21 23:14:42 +0300

    added browser notification

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -223,9 +223,21 @@ FriendlyChat.prototype.checkSignedInWithMessage = function() {
   return false;
 };
 
-// Saves the messaging device token to the datastore.
-FriendlyChat.prototype.saveMessagingDeviceToken = function() {
-  // TODO(DEVELOPER): Save the device token in the realtime datastore
+//saves the messaging device token to the datastore
+FriendlyChat.prototype.saveMessagingDeviceToken = function () {
+  firebase.messaging().getToken().then(function (currentToken) {
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to the datastore.
+      firebase.database().ref('/fcmTokens').child(currentToken)
+        .set(firebase.auth().currentUser.uid);
+    } else {
+      // Need to request permissions to show notifications.
+      this.requestNotificationsPermissions();
+    }
+  }.bind(this)).catch(function (error) {
+    console.error('Unable to get messaging token.', error);
+  });
 };
 
 // Requests permissions to show notifications.
